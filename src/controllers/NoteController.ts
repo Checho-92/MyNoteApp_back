797638858a5423c6260a49f123da8841267e9a0c
Checho-92 <sergio.smt92@gmail.com>
2024-07-09 01:24:29 -0500

    Varias actualizaciones

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,31 +1,28 @@
 // src/controllers/NoteController.ts
 import { Request, Response } from 'express';
 import NoteService from '../applications/services/NoteService';
+import { Note } from '../domain/models/Note';
 
 // Clase NoteController que maneja las solicitudes HTTP relacionadas con las notas
 class NoteController {
   // Método para manejar la creación de una nueva nota
   async create(req: Request, res: Response) {
     try {
-      const { nombre, contenido, id_usuario, estado, fecha } = req.body;
-      if (!nombre) {
-        return res.status(400).json({ message: 'El título de la nota es obligatorio.' });
-      }
-      if (!contenido) {
-        return res.status(400).json({ message: 'El contenido de la nota es obligatorio.' });
-      }
-      if (!id_usuario) {
-        return res.status(400).json({ message: 'El ID del usuario es obligatorio.' });
-      }
-      if (!estado) {
-        return res.status(400).json({ message: 'El estado de la nota es obligatorio.' });
-      }
-      if (!fecha) {
-        return res.status(400).json({ message: 'La fecha de la nota es obligatoria.' });
+      const { nombre, contenido, id_usuario } = req.body;
+      if (!nombre || !contenido || !id_usuario) {
+        return res.status(400).json({ message: 'Nombre, contenido y id_usuario son obligatorios.' });
       }
 
-      const note = await NoteService.createNote(req.body); // Llama al servicio para crear la nota
-      res.status(201).json(note); // Retorna la nota creada con un estado 201 (creado)
+      const note: Partial<Note> = {
+        nombre,
+        contenido,
+        id_usuario,
+        estado: req.body.estado || 'Pendiente',
+        fecha: new Date(),
+      };
+
+      const createdNote = await NoteService.createNote(note as Note); // Llama al servicio para crear la nota
+      res.status(201).json(createdNote); // Retorna la nota creada con un estado 201 (creado)
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ message: `Error al crear la nota: ${error.message}` }); // Retorna un error 500 si algo falla
@@ -58,20 +55,12 @@ class NoteController {
   async update(req: Request, res: Response) {
     try {
       const { id_nota, nombre, contenido, estado } = req.body;
-      if (!id_nota) {
-        return res.status(400).json({ message: 'El ID de la nota es obligatorio.' });
-      }
-      if (!nombre) {
-        return res.status(400).json({ message: 'El título de la nota es obligatorio.' });
-      }
-      if (!contenido) {
-        return res.status(400).json({ message: 'El contenido de la nota es obligatorio.' });
-      }
-      if (!estado) {
-        return res.status(400).json({ message: 'El estado de la nota es obligatorio.' });
+      if (!id_nota || !nombre || !contenido || !estado) {
+        return res.status(400).json({ message: 'ID de nota, nombre, contenido y estado son obligatorios.' });
       }
 
-      await NoteService.updateNote(req.body); // Llama al servicio para actualizar la nota
+      const note: Note = { id_nota, nombre, contenido, estado, id_usuario: req.body.id_usuario, fecha: req.body.fecha };
+      await NoteService.updateNote(note); // Llama al servicio para actualizar la nota
       res.status(200).json({ message: 'Nota actualizada exitosamente' }); // Retorna un mensaje de éxito
     } catch (error) {
       if (error instanceof Error) {
